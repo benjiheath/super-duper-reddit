d06983f6e0a3ee22e12607e8710e543e54cf3a6c
Benjamin Heath <80618413+benjiheath@users.noreply.github.com>
2021-12-18 19:40:54 +1000

    feat(db): now using compose for local db

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM postgres
-ENV POSTGRES_PASSWORD $DB_PW
-ENV POSTGRES_DB srdb
-ENV POSTGRES_HOST_AUTH_METHOD trust
-COPY srdb.sql /docker-entrypoint-initdb.d/
--- src/server/config.ts
+++ src/server/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   pg: {
     user: process.env.DB_USER || 'postgres',
-    pw: (process.env.DB_PW as string) || 'pgdocker',
+    pw: (process.env.DB_PW as string) || 'pass',
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5433,
     database: process.env.DB_NAME || 'srdb',
-    conString: process.env.DB_CONSTRING || 'postgres://postgres:pgdocker@localhost:5433/srdb',
+    conString: process.env.DB_CONSTRING || 'postgres://postgres:pass@localhost:5433/srdb',
   },
   urls: {
     client: {
