66cb5715a7e7ec4c730771331995fed8c9c03a76
Ben Heath <benjamin.d.heath@outlook.com>
2021-12-26 19:36:15 +1000

    feat(createPost): update createPost handler for manual testing

--- src/server/controllers/createPost.ts
+++ src/server/controllers/createPost.ts
@@ -12,7 +12,15 @@ export const createPost: RequestHandler = async (req, res, next) => {
   try {
     const { creator, title, body } = req.body;
 
-    await dbQuery(DbTables.posts).insertRow({ creator, title, body });
+    const userQuery = dbQuery(DbTables.users);
+    const PostsQuery = dbQuery(DbTables.threads);
+
+    const creator_user_id = await userQuery.findValue('id', 'username', creator);
+
+    console.log({ creator });
+    console.log({ creator_user_id });
+
+    await PostsQuery.insertRow({ creator_user_id, title, body });
 
     res.status(200).send({ data: 'posted' });
   } catch (err) {
