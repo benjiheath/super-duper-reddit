6dc3514099c6ad40190c6b52d33e2033043b0d37
Ben Heath <benjamin.d.heath@outlook.com>
2021-12-28 16:36:16 +1000

    style(AlertPop): reduce field error height

--- src/client/components/register/AlertPop.tsx
+++ src/client/components/register/AlertPop.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import { Alert, AlertIcon, AlertTitle } from '@chakra-ui/react';
 
 interface Props {
-    title: string;
+  title: string;
 }
 
-export default function AlertPop({ title}: Props) {
+export default function AlertPop({ title }: Props) {
   return (
-    <Alert status='error' borderRadius={6}>
+    <Alert status='error' borderRadius={6} height={10}>
       <AlertIcon />
       <AlertTitle mr={2} fontSize={12}>
         {title}
