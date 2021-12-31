2e386efd867b3528fd99da2793142fdeaf1e8fd6
Ben Heath <benjamin.d.heath@outlook.com>
2021-12-31 19:14:08 +1000

    style(AlertPop): change color to primary colors

--- src/client/components/register/AlertPop.tsx
+++ src/client/components/register/AlertPop.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export default function AlertPop({ title }: Props) {
   return (
-    <Alert status='error' borderRadius={6} height={10}>
-      <AlertIcon />
-      <AlertTitle mr={2} fontSize={12}>
+    <Alert status='error' borderRadius={6} height={8} bg='prim.50'>
+      <AlertIcon color='prim.800' />
+      <AlertTitle mr={2} fontSize={12} color='prim.800'>
         {title}
       </AlertTitle>
     </Alert>
