b06623995fab2289091a59033d8968619d8ab1b6
Ben Heath <benjamin.d.heath@outlook.com>
2022-01-01 16:41:16 +1000

    style(navbar): add inner maxW to navbar

--- src/client/components/homepage/Nav/NavBar.tsx
+++ src/client/components/homepage/Nav/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Icon, Spacer } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Icon, Spacer } from '@chakra-ui/react';
 import { FaArrowLeft, FaPlus } from 'react-icons/fa';
 import { Link, useHistory } from 'react-router-dom';
 import UserMenu from './UserMenu';
@@ -12,19 +12,21 @@ const NavBar = () => {
       : ['/posts/create', 'secondary', 'Create Post', FaPlus, 'white'];
 
   return (
-    <Flex position='sticky' top={0} zIndex={1} bg='white' p='20px 30px' mb='80px'>
-      <Link to='/'>
-        <Heading color='prim.800'>Super Reddit</Heading>
-      </Link>
-      <Spacer />
-      <Link to={linkTo}>
-        <Button mr={4} variant={btnVariant}>
-          <Icon as={btnIcon} mr={2} />
-          {btnText}
-        </Button>
-      </Link>
-      <UserMenu />
-    </Flex>
+    <Box position='sticky' top={0} zIndex={1} bg='white' mb={8}>
+      <Flex p='20px 0' w='800px' m='0 auto'>
+        <Link to='/'>
+          <Heading color='prim.800'>Super Reddit</Heading>
+        </Link>
+        <Spacer />
+        <Link to={linkTo}>
+          <Button mr={4} variant={btnVariant}>
+            <Icon as={btnIcon} mr={2} />
+            {btnText}
+          </Button>
+        </Link>
+        <UserMenu />
+      </Flex>
+    </Box>
   );
 };
 
