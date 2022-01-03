f47413e9ffe13a2f9b1ff39c3dd3f9f2887a0000
Ben Heath <benjamin.d.heath@outlook.com>
2022-01-03 18:24:35 +1000

    refactor(types): consolidate response types etc into common/fetching

---
+++ src/common/types/fetching.ts
@@ -0,0 +1,57 @@
+import { PostType } from './entities';
+
+export type Endpoint =
+  | 'session'
+  | 'user'
+  | 'posts'
+  | 'account'
+  | 'account/:token'
+  | 'posts/comments'
+  | 'user/account';
+
+type Auth = { auth: boolean };
+
+export type FieldErrorData = { field: string; message: string };
+
+export interface FieldErrorResponse {
+  message: string;
+  errors: FieldErrorData[];
+}
+
+export type RegisterResponse = StatusAndMessage & { errors?: FieldErrorData[] };
+
+export type LoginResponse = StatusAndMessage & Auth & { errors?: FieldErrorData[] };
+
+export type RecoveryResponse = StatusAndMessage & { sentTo: string } & { error?: FieldErrorData };
+
+export type PwResetResponse = StatusAndMessage & { username: string };
+
+export type SessionInfo = Auth & { userID: string | null };
+
+export interface StatusAndMessage {
+  status: 'fail' | 'success';
+  message?: string;
+}
+
+export interface ServerResponse extends StatusAndMessage {
+  errors?: FieldErrorData[];
+  error?: FieldErrorData;
+  auth?: boolean;
+  sentTo?: string;
+  username?: string;
+  userID?: string | null;
+  post?: PostType;
+  posts?: PostType[];
+}
+
+export type PostResponse = { post: PostType };
+export type PostsResponse = { posts: PostType[] };
+
+export interface GetPostsResponse {
+  posts: PostType[];
+}
+
+export interface CreatePostResponse {
+  status: string;
+  post: PostType;
+}
