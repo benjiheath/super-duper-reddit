7ebcd2938a1af6240a65659ffdff56320cf7c3d8
Benjamin Heath <80618413+benjiheath@users.noreply.github.com>
2022-01-27 08:01:14 +1000

    add queryClient & provider

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, Flex } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import ScrollToTop from './components/generic/ScrollToTop';
 import PostsProvider from './contexts/posts/PostsContext';
@@ -7,33 +8,45 @@ import { AccountRecovery, Posts, Register } from './pages';
 import NotFound from './pages/NotFound';
 import { theme } from './theme';
 
-export const App = () => (
-  <Router>
-    <ScrollToTop />
-    <Switch>
-      <GlobalUserProvider>
-        <ChakraProvider theme={theme}>
-          <PostsProvider>
-            <Route exact path='/'>
-              <Redirect to='/posts' />
-            </Route>
-            <Route path='/posts'>
-              <Posts />
-            </Route>
-          </PostsProvider>
-          <Flex minH='100vh' alignItems='center'>
-            <Route exact path={['/register', '/login']}>
-              <Register />
-            </Route>
-            <Route path='/reset-password'>
-              <AccountRecovery />
-            </Route>
-            <Route exact path='/404'>
-              <NotFound />
-            </Route>
-          </Flex>
-        </ChakraProvider>
-      </GlobalUserProvider>
-    </Switch>
-  </Router>
-);
+export const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  return (
+    <Router>
+      <ScrollToTop />
+      <Switch>
+        <GlobalUserProvider>
+          <ChakraProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+              <PostsProvider>
+                <Route exact path='/'>
+                  <Redirect to='/posts' />
+                </Route>
+                <Route path='/posts'>
+                  <Posts />
+                </Route>
+              </PostsProvider>
+              <Flex minH='100vh' alignItems='center'>
+                <Route exact path={['/register', '/login']}>
+                  <Register />
+                </Route>
+                <Route path='/reset-password'>
+                  <AccountRecovery />
+                </Route>
+                <Route exact path='/404'>
+                  <NotFound />
+                </Route>
+              </Flex>
+            </QueryClientProvider>
+          </ChakraProvider>
+        </GlobalUserProvider>
+      </Switch>
+    </Router>
+  );
+};
