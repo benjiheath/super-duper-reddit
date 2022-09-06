1f21a1c426016f8892dd814eeb83714e1d472e60
Ben Heath <benjamin.d.heath@outlook.com>
2022-09-06 18:08:15 +1000

    refactor(accountRecovery): refactor form and add hook for mutation

--- src/client/components/AccountRecovery/RecoveryEmailForm.tsx
+++ src/client/components/AccountRecovery/RecoveryEmailForm.tsx
@@ -1,63 +1,26 @@
-import { Input, useToast, VStack } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { FaArrowLeft } from 'react-icons/fa';
-import { ServerResponse } from '../../../common/types';
-import { useAuthContext } from '../../contexts/user/AuthContext';
-import { RecoveryEmailFormData } from '../../types/user';
-import { axiosPOST } from '../../utils/axiosMethods';
+import { useRecoverAccountMutation } from '../../hooks/mutations/useRecoverAccountMutation';
+import { useSuccessToast } from '../../hooks/useSrToast';
+import { Input, VStack } from '@chakra-ui/react';
 import { obscureEmail } from '../../utils/misc';
+import { FaArrowLeft } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
 import ButtonSubmit from '../generic/ButtonSubmit';
-import FormBox from '../generic/FormBox';
 import RoutingLink from '../generic/RoutingLink';
 import AlertPop from '../resisterAndLogin/AlertPop';
+import FormBox from '../generic/FormBox';
 
 export default function RecoveryEmailForm() {
-  const toast = useToast();
-  const [loading, setLoading] = useState(false);
-  const { setResponseError } = useAuthContext();
+  const successToast = useSuccessToast();
+  const methods = useForm();
+  const recoverAccountMutation = useRecoverAccountMutation(methods.setError);
 
-  const {
-    register,
-    reset,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm();
+  const { errors } = methods.formState;
 
-  const onSubmit = async ({ id }: RecoveryEmailFormData): Promise<void> => {
-    setLoading(true);
-    const idType = id.includes('@') ? 'email' : 'username';
+  const onSubmit = async (data: { id: string }): Promise<void> => {
+    const res = await recoverAccountMutation.mutateAsync(data.id);
 
-    try {
-      const res = await axiosPOST<ServerResponse<string>>('user/account', {
-        data: { [idType]: id },
-      });
-
-      // TODO need guard clause for atypical/unexpected errors. if so, setResponseError to something generic that CTX can handle, then return
-
-      if (res?.errors) {
-        const [{ message }] = res.errors;
-        setError('id', { message });
-        setLoading(false);
-        return;
-      }
-
-      if (res.data) {
-        toast({
-          title: `Email sent to ${obscureEmail(res.data)}`,
-          status: 'success',
-          duration: 6000,
-          isClosable: true,
-          position: 'top',
-          variant: 'srSuccess',
-        });
-      }
-    } catch (err) {
-      setResponseError(err);
-    }
-    reset();
-    setLoading(false);
+    successToast(`Email sent to ${obscureEmail(res)}`, { duration: 6000 });
+    methods.reset();
   };
 
   return (
@@ -67,27 +30,29 @@ export default function RecoveryEmailForm() {
       subTitle='Enter your username or email to recieve a recovery link.'
     >
       <VStack mt={10} spacing='3px'>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
           <VStack w='260px'>
             <Input
               focusBorderColor='sec.800'
               type='text'
               placeholder='Username or email'
-              {...register('id', {
+              {...methods.register('id', {
                 required: 'Please specify your username or email address',
                 minLength: { value: 4, message: 'Usernames must have at least 4 characters' },
               })}
+              mb={4}
             />
             {errors.id && <AlertPop title={errors.id.message} />}
             <ButtonSubmit
               text='Send email'
               colorScheme='green'
-              m='35px 0 0'
-              loading={loading}
+              loading={recoverAccountMutation.isLoading}
               loadingText='Sending'
               variant='secondary'
             />
-            {!loading && <RoutingLink to='/login' text='Back to login' icon={FaArrowLeft} subtle />}
+            {!recoverAccountMutation.isLoading && (
+              <RoutingLink to='/login' text='Back to login' icon={FaArrowLeft} subtle />
+            )}
           </VStack>
         </form>
       </VStack>
