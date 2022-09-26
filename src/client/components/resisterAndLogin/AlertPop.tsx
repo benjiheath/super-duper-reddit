43a11f70d663f9d51bb997cf1e48ebdf7b63f015
Ben Heath <benjamin.d.heath@outlook.com>
2022-09-26 16:46:44 +1000

    style(forms): allow validation alerts to occupy space as required

--- src/client/components/resisterAndLogin/AlertPop.tsx
+++ src/client/components/resisterAndLogin/AlertPop.tsx
@@ -1,5 +1,4 @@
 import { Alert, AlertIcon, AlertProps, AlertTitle } from '@chakra-ui/react';
-import { primaryColors } from '../../theme';
 
 interface Props extends AlertProps {
   title: string;
@@ -8,7 +7,7 @@ interface Props extends AlertProps {
 export default function AlertPop(props: Props) {
   const { title, ...rest } = props;
   return (
-    <Alert status='error' borderRadius={6} height={8} bg='prim.50' {...rest}>
+    <Alert status='error' borderRadius={6} bg='prim.50' p={1} {...rest}>
       <AlertIcon color='prim.800' />
       <AlertTitle mr={2} fontSize={12} color='prim.800'>
         {title}
